434d930b808d5c975e821861a1f3e6dbf067cf7e
raydabbah <dabbahray@yahoo.com>
2017-07-14 02:48:34 -0400

    initial commit

---
+++ callBackFun.js
@@ -0,0 +1,27 @@
+setTimeout(function(){
+    console.log('one')
+    setTimeout(function(){
+        console.log('two')
+        setTimeout(function(){
+            console.log('three')
+        },1000);
+    },1000);
+}, 1000);
+
+function four(cb){
+    setTimeout(function(){
+        console.log('four')
+    }, 1000)
+    setTimeout(cb, 1000);
+}
+function five(cb){
+    console.log('five');
+    setTimeout(cb, 1000);
+}
+function six(cb){
+    console.log('six');
+}
+
+four(function(){
+    five(six)
+})
---
+++ program.js
@@ -0,0 +1,11 @@
+var pro = require('es6-promise')
+
+
+new Promise(function(function(fulfill, reject){
+	
+}){
+
+})
+setTimeout(function(){
+	console.log('TIMED OUT!')
+}, 300)
