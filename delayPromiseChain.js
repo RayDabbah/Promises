d25feeaaa8f0150845117b27d2db25e571b294e5
raydabbah <dabbahray@yahoo.com>
2017-07-16 20:56:28 -0400

    thunks that work!

---
+++ delayPromiseChain.js
@@ -0,0 +1,37 @@
+function delay(num) {
+    return new Promise(function(resolve, error){
+    setTimeout(resolve, num)
+    });
+}
+
+delay(100)
+    .then(function() {
+        console.log('first then')
+        return delay(1052);
+    })
+    .then(function(){
+        console.log('second then')
+        return delay(1106);
+    })
+    .then(function(){
+        console.log('all done');
+    })
+
+    function arithmetic(number){
+        return new Promise(function(resolve, reject){
+            setTimeout(function(){resolve(number)}, 1000);
+        })
+    }
+
+    arithmetic(26)
+    .then(function(num1){
+        x = num1 + 2;
+        return arithmetic(52);
+    })
+    .then(function(num2){
+        y = num2 * 2;
+        return arithmetic(y + x);
+    })
+    .then(function(num3){
+        console.log(num3);
+    })
---
+++ thunks.js
@@ -0,0 +1,49 @@
+function fakeAjax(url, cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function () {
+		cb(fake_responses[url]);
+	}, randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+		var response;
+	fakeAjax(file, function (text) {
+		if(!response) response = text;
+		else response(text);
+	})
+	return function th(cb) {
+		if(response) cb(response);
+		 else response = cb
+	}
+}
+
+
+
+var th1 = getFile('file1')
+var th2 = getFile('file2')
+var th3 = getFile('file3')
+
+th1(function (text1) {
+	output(text1);
+	th2(function (text2) {
+		output(text2);
+		th3(function (text3) {
+			output(text3);
+			output('Completed!!!')
+		})
+	})
+})
